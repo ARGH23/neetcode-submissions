8392be6c1d49584541d10046f427ad95419af44e
Agastya Govind <79813984+ARGH23@users.noreply.github.com>
2026-09-16 16:56:34 -0500

    Add: count-number-of-islands - submission-4

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-4.py
@@ -0,0 +1,34 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        islands = 0
+        visited = {}
+
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                #print(visited)
+                if (i, j) not in visited and int(grid[i][j]) != 0:
+                    #print(i,j)
+                    islands += 1
+
+                    queue = []
+                    queue.append([i,j])
+                    visit = {}
+
+                    while queue:
+                        cur = queue.pop()
+                        
+
+                        if int(grid[cur[0]][cur[1]]) == 1 and (cur[0], cur[1]) not in visit:
+                            if cur[0] - 1 >= 0:
+                                queue.append([cur[0]-1,cur[1]])
+                            if cur[0] + 1 < len(grid):
+                                queue.append([cur[0]+1,cur[1]])
+                            if cur[1] - 1 >= 0:
+                                queue.append([cur[0],cur[1]-1])
+                            if cur[1] + 1 < len(grid[0]):
+                                queue.append([cur[0],cur[1]+1])
+                        
+                        visit[(cur[0], cur[1])] = True
+                        visited[(cur[0], cur[1])] = True
+        
+        return islands
