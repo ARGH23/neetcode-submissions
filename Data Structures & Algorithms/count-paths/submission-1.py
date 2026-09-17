dd9142bbf2cadeb3de4b761eeab37b810e7f5011
Agastya Govind <79813984+ARGH23@users.noreply.github.com>
2026-09-17 17:07:49 -0500

    Add: count-paths - submission-1

---
+++ Data Structures & Algorithms/count-paths/submission-1.py
@@ -0,0 +1,29 @@
+import math
+
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        # grid = []
+
+        # for i in range(m):
+        #     row = []
+        #     for j in range(n):
+        #         row.append(0)
+        #     grid.append(row)
+        
+        # grid[m-1][n-1] = 1
+
+        # #grid[i][j] = grid[i+1][j] + grid[i][j+1]
+
+
+        # for i in range(m-1, -1, -1):
+        #     for j in range(n-1, -1, -1):
+        #         total = grid[i][j]
+        #         if i+1 < m:
+        #             total += grid[i+1][j]
+        #         if j+1 < n:
+        #             total += grid[i][j+1]
+        #         grid[i][j] = total
+        
+        # return grid[0][0]
+
+        return int(math.factorial(m+n-2)/(math.factorial(m-1)*math.factorial(n-1)))
